2d312dbe8aeb197b889812dda9df26d7b563fd72
Max Scher and Nathan Stewart <max@>
2017-09-25 16:40:50 -0700

    include access-token from postman in notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { getBearerToken } from './../actions';
+import twitter from './../../twitter.config';
 
 function App(){
+
   return(
     <div>
       <h1>App working</h1>
